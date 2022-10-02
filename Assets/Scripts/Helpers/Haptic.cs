2124a353607b5b53c4cf20ceeaebeac490ac4e8c
Yunus Emre <yunusemre@gui.com.tr>
2022-10-02 20:48:26 +0300

    +added: haptic class

---
+++ Assets/Scripts/Helpers/Haptic.cs
@@ -0,0 +1,37 @@
+﻿using MoreMountains.NiceVibrations;
+
+namespace Helpers
+{
+    public static class Haptic
+    {
+        public static void Success()
+        {
+            MMVibrationManager.Haptic(HapticTypes.Success);
+        }
+        
+        public static void Fail()
+        {
+            MMVibrationManager.Haptic(HapticTypes.Failure);
+        }
+        
+        public static void Soft()
+        {
+            MMVibrationManager.Haptic(HapticTypes.SoftImpact);
+        }
+        
+        public static void Medium()
+        {
+            MMVibrationManager.Haptic(HapticTypes.MediumImpact);
+        }
+        
+        public static void Heavy()
+        {
+            MMVibrationManager.Haptic(HapticTypes.HeavyImpact);
+        }
+        
+        public static void Selection()
+        {
+            MMVibrationManager.Haptic(HapticTypes.HeavyImpact);
+        }
+    }
+}
