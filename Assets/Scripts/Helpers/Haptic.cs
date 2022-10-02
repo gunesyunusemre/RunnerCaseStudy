c7cd39541677eddc837a6f0075858ba4271a3e67
Yunus Emre <yunusemre@gui.com.tr>
2022-10-02 20:56:54 +0300

    +added: haptic threshold
    when haptic(success, soft, fail etc) play continous then fps drop

--- Assets/Scripts/Helpers/Haptic.cs
+++ Assets/Scripts/Helpers/Haptic.cs
@@ -1,37 +1,63 @@
 ﻿using MoreMountains.NiceVibrations;
+using UnityEngine;
 
 namespace Helpers
 {
     public static class Haptic
     {
+        private static float _hapticTime = 0;
+        private static float _threshold = .5f;
+        
         public static void Success()
         {
+            if (CheckHapticThreshold())
+                return;
+
             MMVibrationManager.Haptic(HapticTypes.Success);
         }
         
         public static void Fail()
         {
+            if (CheckHapticThreshold())
+                return;
             MMVibrationManager.Haptic(HapticTypes.Failure);
         }
         
         public static void Soft()
         {
+            if (CheckHapticThreshold())
+                return;
             MMVibrationManager.Haptic(HapticTypes.SoftImpact);
         }
         
         public static void Medium()
         {
+            if (CheckHapticThreshold())
+                return;
             MMVibrationManager.Haptic(HapticTypes.MediumImpact);
         }
         
         public static void Heavy()
         {
+            if (CheckHapticThreshold())
+                return;
             MMVibrationManager.Haptic(HapticTypes.HeavyImpact);
         }
         
         public static void Selection()
         {
+            if (CheckHapticThreshold())
+                return;
             MMVibrationManager.Haptic(HapticTypes.HeavyImpact);
+        }
+
+        private static bool CheckHapticThreshold()
+        {
+            var check = Time.time - _hapticTime < _threshold;
+            if (!check)
+                _hapticTime = Time.time;
+            
+            return check;
         }
     }
 }
